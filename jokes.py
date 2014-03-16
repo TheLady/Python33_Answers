5d03c582c98df13ea0ef719b5223e76dc243d7dd
TheLady <beta@natashascourt.com>
2014-03-16 05:35:44 -0400

    Create jokes.py
    The Python 3 tutorial did not eliminate the end= or the print() commands. 
    
    Replaced input with raw_input.

---
+++ jokes.py
@@ -0,0 +1,11 @@
+print('How does NASA organize their company parties?')
+raw_input()
+print('they planet''\n')
+print('What kind of shoes do ninjas wear?')
+raw_input()
+print('sneakers''\n')
+print('you want to hear a pizza joke?')
+raw_input()
+print('Nevermind it\'s pretty Cheesy')
+
+#updated python 3.3 code from http://www.youtube.com/watch?v=zFFzSZI6DWI
